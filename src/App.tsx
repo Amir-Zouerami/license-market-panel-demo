8ba2c01b7e2342222e78bc418620bcea5c3e2b2f
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-03 10:40:57 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import React from "react";
+import { Button } from "antd";
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+    <p>تست فونت</p>
+  </div>
+);
+
+export default App;
