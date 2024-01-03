17c67aac2104a42fb64c9884c0aebae631be7d07
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-03 17:26:04 +0330

    LOGIC|ANTD: set `direction` and other defaults on `antd` package.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { ConfigProvider } from "antd";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { RouterProvider } from "react-router-dom";
+import router from "./lib/routing/router";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ConfigProvider
+      direction="rtl"
+      theme={{ token: { fontFamily: "IranSANS", fontSize: 16 } }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  </React.StrictMode>
+);
