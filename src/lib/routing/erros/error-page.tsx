a37c20e48bb6001957c8efac1fe824f17cd3af16
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-03 18:36:18 +0330

    UI|ERRORPAGE: improves the UI for the error page.

--- src/lib/routing/erros/error-page.tsx
+++ src/lib/routing/erros/error-page.tsx
@@ -3,11 +3,12 @@ import { getErrorMessage } from "./router-error-check";
 import E404 from "./components/E404";
 import E403 from "./components/E403";
 import E500 from "./components/E500";
+import { Button } from "antd";
 
 export default function ErrorPage() {
   const error = useRouteError();
   const errorMessage = getErrorMessage(error);
-  console.log(errorMessage);
+  // console.log(errorMessage);
 
   return (
     <div className="panel-error-page-container">
@@ -18,7 +19,12 @@ export default function ErrorPage() {
       ) : errorMessage.status === 500 ? (
         <E500 />
       ) : (
-        <h1>متاسفانه خطایی پیش آمده است.</h1>
+        <>
+          <h1 style={{ margin: "20px 0" }}>متاسفانه خطایی پیش آمده است.</h1>
+          <Button type="primary" href="/">
+            برگشت به صفحه ی اصلی
+          </Button>
+        </>
       )}
     </div>
   );
