a6280f8aead616951bb4bab672edd29363698c31
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-04 10:17:34 +0330

    FEATURE|AUTH: uses `Authorize` on the entire panel to protect from unauthenticated users.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,8 +2,9 @@ import { Menu } from "antd";
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import type { MenuProps } from "antd";
-import NavLinkList from "@/pages/Navbar/NavLinkList";
+import NavLinkList from "@/components/Layout/NavLinkList";
 import styles from "./Layout.module.scss";
+import Authorize from "@/lib/routing/Auth/Authorize";
 
 const Layout = () => {
   const [current, setCurrent] = useState("home");
@@ -14,17 +15,19 @@ const Layout = () => {
 
   return (
     <div dir="rtl">
-      <Menu
-        className={styles.panelNavMenu}
-        onClick={onClick}
-        selectedKeys={[current]}
-        mode="horizontal"
-        items={NavLinkList}
-      />
+      <Authorize>
+        <Menu
+          className={styles.panelNavMenu}
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={NavLinkList}
+        />
 
-      <div className={styles.panelLayout}>
-        <Outlet />
-      </div>
+        <div className={styles.panelLayout}>
+          <Outlet />
+        </div>
+      </Authorize>
     </div>
   );
 };
