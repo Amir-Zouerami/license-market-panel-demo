eddf8a596f47ddc4d2613ac1a782ecc3abc14011
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-06 16:37:57 +0330

    TYPE|USER: better typings related to the user.

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,27 +1,70 @@
 type RoleName = "super admin" | "admin" | "user";
+export type Role = { id: number; name: RoleName };
 
-export interface LoginResponseSuccess {
-  status: string;
+/**
+ * This is **NOT** the data type returned from the API call.
+ *
+ * It is returned from the `/lib/apiHandler/login/login`.
+ */
+interface LoginResponseSuccess {
+  ok: true;
   data: {
-    access_token: string;
-    roles: {
-      id: number;
-      name: RoleName;
+    data: {
+      access_token: string;
+      roles: Role[];
     };
+    status: string;
+    statusCode: number;
+    timestamp: string;
   };
-  statusCode: number;
 }
 
-export interface LoginResponseFail {
-  erros: {
-    message: string;
-    erorr: string;
-    status: number;
-  };
-  timestamp: number;
+/**
+ * This is **NOT** the data type returned from the API call.
+ *
+ * It is returned from the `/lib/apiHandler/login/login`.
+ */
+interface LoginResponseFail {
+  ok: false;
+  data: Error;
 }
+
+export type LoginResponse = LoginResponseFail | LoginResponseSuccess;
+
+// -----------------------
 
 export interface UserLoginCredentials {
   email: string;
   password: string;
 }
+
+// -----------------------
+
+/**
+ * This is **NOT** the data type returned from the API call.
+ *
+ * It is returned from the `/lib/apiHandler/login/assignRole`.
+ */
+interface AssignRoleSuccess {
+  ok: true;
+  data: {
+    data: {
+      access_token: string;
+    };
+    status: string;
+    statusCode: number;
+    timestamp: string;
+  };
+}
+
+/**
+ * This is **NOT** the data type returned from the API call.
+ *
+ * It is returned from the `/lib/apiHandler/login/assignRole`.
+ */
+interface AssignRoleFail {
+  ok: false;
+  data: Error;
+}
+
+export type AssignRole = AssignRoleSuccess | AssignRoleFail;
