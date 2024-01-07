3b231bd89f380ebe99bcc86e8ee1016ebcb17a37
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-07 18:57:17 +0330

    PAGE|ORDERS: the orders page now uses the table component to display orders.

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,7 +1,20 @@
+// import { getRoleName } from "@/lib/localStorage/localStorage";
+import OrdersTable from "@/components/Orders/OrdersTable";
+import styles from "./orders.module.scss";
+
 const Orders = () => {
+  // const role = getRoleName();
+
   return (
-    <div>
-      <p>Orders PAGE</p>
+    <div className={styles.ordersContainer}>
+      <div className={styles.content}>
+        <div>
+          <h1>لیست سفارشات</h1>
+          <p>در این بخش سفارشات را بسته به سطح دسترسی خود مشاهده می کنید.</p>
+        </div>
+
+        <OrdersTable />
+      </div>
     </div>
   );
 };
