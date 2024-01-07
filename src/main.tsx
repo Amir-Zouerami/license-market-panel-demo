bf310f11db5440e38e9e6a620e2a1f1c1fcef0ae
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-07 18:55:00 +0330

    REACT-QUERY: set the `react-query` provider on the entire app to be able to use it.

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import { ConfigProvider } from "antd";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import router from "./lib/routing/router";
+import ReactDOM from "react-dom/client";
+import { ConfigProvider } from "antd";
+import React from "react";
+import "./index.scss";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ConfigProvider
-      direction="rtl"
-      theme={{
-        components: { Form: { itemMarginBottom: 35, labelFontSize: 14 } },
-        token: { fontFamily: "IranSANS", fontSize: 16 },
-      }}
-    >
-      <RouterProvider router={router} />
-    </ConfigProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider
+        direction="rtl"
+        theme={{
+          components: { Form: { itemMarginBottom: 35, labelFontSize: 14 } },
+          token: { fontFamily: "IranSANS", fontSize: 16 },
+        }}
+      >
+        <RouterProvider router={router} />
+      </ConfigProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
