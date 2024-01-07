e53d79d6b7c13d37d523d3d66a2e93ecfdcae732
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-07 18:47:27 +0330

    TYPES|ORDER: defined types for `orders`.

---
+++ src/types/Orders.ts
@@ -0,0 +1,47 @@
+interface OrdersMetadata {
+  total: number;
+  lastPage: number;
+  currentPage: number;
+  perPage: number;
+  prev: number;
+  next: number;
+}
+
+export interface Order {
+  id: number;
+  orderNumber: string;
+  orderTime: string;
+  dueTime: string;
+  deleted: null;
+  assignee: number;
+  productId: number;
+  userId: number;
+}
+
+interface PaginatedOrdersSuccess {
+  timestamp: string;
+  status: string;
+  data: {
+    data: Order[];
+    meta: OrdersMetadata;
+  };
+  statusCode: number;
+}
+
+interface PaginatedOrdersFail {
+  errors: {
+    message: string;
+    error: string;
+    statusCode: number;
+  };
+  timestamp: number;
+}
+
+export type PaginatedOrders = PaginatedOrdersSuccess | PaginatedOrdersFail;
+
+export const responseIsError = (
+  response: PaginatedOrders
+): response is PaginatedOrdersFail => {
+  if ("errors" in response) return true;
+  return false;
+};
