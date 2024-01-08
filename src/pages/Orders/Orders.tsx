2e26f64504e084c9d547be69101101f7ce2723b6
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-08 13:51:44 +0330

    UI|ORDERS: `Orders` page now uses the `<PageLayout />` wrapper.

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,18 +1,14 @@
 import OrdersTable from "@/components/Orders/OrdersTable";
-import styles from "./orders.module.scss";
+import PageLayout from "@/components/Common/Page/PageLayout";
 
 const Orders = () => {
   return (
-    <div className={styles.ordersContainer}>
-      <div className={styles.content}>
-        <div>
-          <h1>لیست سفارشات</h1>
-          <p>در این بخش سفارشات را بسته به سطح دسترسی خود مشاهده می کنید.</p>
-        </div>
-
-        <OrdersTable />
-      </div>
-    </div>
+    <PageLayout
+      title="لیست سفارشات"
+      description="در این بخش سفارشات را بسته به سطح دسترسی خود مشاهده می کنید."
+    >
+      <OrdersTable />
+    </PageLayout>
   );
 };
 
