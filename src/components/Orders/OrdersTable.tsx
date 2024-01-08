2b1213f1c5954f1e4c7ca61e9b20c5874e4573c6
Amir Zouerami <53701884+Amir-Zouerami@users.noreply.github.com>
2024-01-08 10:59:47 +0330

    UI|ORDERS: the `OrdersTable` now uses the common table component.

--- src/components/Orders/OrdersTable.tsx
+++ src/components/Orders/OrdersTable.tsx
@@ -1,41 +1,24 @@
 import { useGetOrders } from "@/lib/apiHandlers/Orders/GetAllOrders";
-import { addKeyToOrders } from "@/lib/utility/addKeyToOrders";
+import CommonTable from "@/components/Common/CommonTable";
 import { getOrderTableColumns } from "./TableColumns";
-import GetOrdersFailed from "./GetOrdersFailed";
 import TableSearch from "./TableSearch";
 import React, { useState } from "react";
-import { Table } from "antd";
 
 const OrdersTable: React.FC = () => {
-  const ROWS_PER_PAGE = 2;
   const [page, setPage] = useState(1);
-  const { data, isPending, error } = useGetOrders(page, ROWS_PER_PAGE);
+  const { data, isPending, error } = useGetOrders(page);
   const columns = getOrderTableColumns(TableSearch());
 
   return (
-    <>
-      {error && error.message === "NOT AUTHORIZED" ? (
-        <GetOrdersFailed type="unauthorized" />
-      ) : error ? (
-        <GetOrdersFailed type="unknown" />
-      ) : (
-        <Table
-          columns={columns}
-          dataSource={data ? addKeyToOrders(data.data.data) : []}
-          loading={isPending}
-          pagination={{
-            position: ["bottomRight"],
-            pageSize: ROWS_PER_PAGE,
-            current: page,
-            total: data ? data.data.meta.lastPage + 1 : 1,
-            onChange: page => {
-              setPage(page);
-            },
-          }}
-          scroll={{ x: 800 }}
-        />
-      )}
-    </>
+    <CommonTable
+      page={page}
+      totalPages={data?.data.meta.lastPage}
+      setPage={setPage}
+      data={data?.data.data}
+      isPending={isPending}
+      error={error}
+      columns={columns}
+    />
   );
 };
 
